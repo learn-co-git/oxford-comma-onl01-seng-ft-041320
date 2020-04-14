5d069c1117af329ba9635089d555cc9b8136a5a6
Neil Empey <argus.two.2@gmail.com>
2020-04-14 21:18:52 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+	arr = []
+	array.each_with_index do |ele, i|
+		str = ''
+		if(ele == array[array.length - 1])
+			str = "and " + ele
+			arr << str
+		else
+			str = ele + ", "
+			arr << str
+		end
+	end
+		puts arr.join()
 end
