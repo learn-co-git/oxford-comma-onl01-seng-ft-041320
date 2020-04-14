c41b79c5a3efd9df3829f189ce23e4c17c45a6e6
Neil Empey <argus.two.2@gmail.com>
2020-04-14 21:33:23 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
 	str = ''
 	if(array.length == 1)
 		return array[0]
-	elsif(array.length = 2)
+	elsif(array.length == 2)
 		str = array[0] + "and" + array[1]
 		return str
 	end
